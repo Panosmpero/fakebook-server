0877857d7dbb444dacd7e71f527f2ae1771392b1
Beros <59673191+Panosmpero@users.noreply.github.com>
2021-04-28 21:09:32 +0100

    server, mongoose setup - added nodemon

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 5000;
+const { MONGO_URI } = require("./config");
+const Post = require("./models/Post");
+const User = require("./models/User");
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    username: String!
+    createdAt: String!
+  }
   type Query {
-    hello: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "Hello from GraphQL!",
+    getPosts: async () => {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
   },
 };
 
@@ -20,6 +37,25 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server
-  .listen({ port: PORT })
-  .then((res) => console.log(`Server running on port ${res.url}`));
+// mongoose.connect(
+//   MONGO_URI,
+//   { useNewUrlParser: true, useUnifiedTopology: true },
+//   (err, client) => {
+//     if (err) return console.log("MongoDB error");
+//     console.log("MongoDB connected");
+//   }
+// );
+
+// server
+//   .listen({ port: PORT })
+//   .then((res) => console.log(`Server running on port ${res.url}`));
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected");
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`Server running on port ${res.url}`);
+  });
